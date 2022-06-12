d2933340cc688297c0a1c75203ef51a440232d68
AzizRahmawan <arder058@gmail.com>
2022-06-12 22:14:18 +0700

    .

--- website_profile_fix/routes/web.php
+++ website_profile_fix/routes/web.php
@@ -14,9 +14,7 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', function () {
-    return view('backend.index');
-});
+Route::get('/', '\App\Http\Controllers\Backend\IndexController');
 
 Auth::routes([
     'register' => false, // Register Routes...
